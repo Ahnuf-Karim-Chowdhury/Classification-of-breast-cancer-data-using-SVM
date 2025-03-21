b47d5a4d27a278314440d903dbc59927efb479c5
ahnuf <ahnufkarimchowdhury@gmail.com>
2025-03-22 03:54:07 +0600

    SVM Added

---
+++ Approach - 01 - Training it Newly Every Iteration/SVM.py
@@ -0,0 +1,32 @@
+import sklearn
+from sklearn import datasets
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import matplotlib.pyplot as plt
+
+cancer = datasets.load_breast_cancer()
+
+x = cancer.data
+y = cancer.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+classes = ['malignant', 'benign']
+
+clf = svm.SVC(kernel='linear', C=2)
+clf.fit(x_train, y_train)
+
+y_pred = clf.predict(x_test)
+
+accuracy = metrics.accuracy_score(y_test, y_pred)
+print(accuracy)
+
+plt.figure(figsize=(10,6))
+plt.plot(range(len(y_test)), y_test, label="Actual", marker="o", linestyle="--", color="blue")
+plt.plot(range(len(y_test)), y_pred, label="Predicted", marker="x", linestyle="-", color="red")
+plt.title(f"SVM Classification: Accuracy = {accuracy*100:.2f}%")
+plt.xlabel("Test Data Index")
+plt.ylabel("Class (0=Malignant, 1=Benign)")
+plt.legend()
+plt.show()
